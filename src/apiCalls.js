d5a92d77fba5450b6e71f793b411317946b673a9
Taras Tarlov <ttarlov@gmail.com>
2020-06-05 15:22:15 -0600

    Remove used function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,6 @@
 export const decodeVin = async (vin, year) => {
- 
+
+    
  try {
     const result = await fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${vin}?format=json&modelyear=${year}`)
 
@@ -8,41 +9,10 @@ export const decodeVin = async (vin, year) => {
         throw new Error(`Problem received status code of ${result.status}`)
     }
         const response = await result.json() 
-       // console.log(response.Results)
+    //    console.log(response.Results)
     return response.Results
     } catch (error) {
     window.alert(`Server Error. It's not your fault the error is: ${error}`)
     }
 
 }
-
-   
-   
-   const buildCar = async (vinDecodeResponse) => {
-   
-   const responseData = await vinDecodeResponse
-   
-   const initialCar = responseData.reduce((carData, item) => {
-   
-    
-     if(item.Variable === carData[item.Variable]) {
-       carData[item.Variable] = item.Value
-     }
-   
-    
-     return carData
-   },{
-     "Model Year": "Model Year", 
-     "Make": "Make", 
-     "Model": "Model", 
-     "Trim": "Trim",
-    "Series": "Series",
-     "Displacement (L)": "Displacement (L)",
-     "Engine Brake (hp)": "Engine Brake (hp)",
-     })
-   
-   console.log(initialCar)
-   
-   
-   
-   }
