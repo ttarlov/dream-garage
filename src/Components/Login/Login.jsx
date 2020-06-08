7aa85e46ef92bd15ef5f17813d45e1d2d724f787
Taras Tarlov <ttarlov@gmail.com>
2020-06-08 12:44:41 -0600

    Refactor Login Comp to be fn Comp

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,75 +2,38 @@ import React, { Component } from 'react';
 // import { Switch, Route, Redirect, L} from 'react-router-dom';
 import './Login.css';
 import { decodeVin } from '../../apiCalls'
-
-class Login extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            year: "",
-            vin: "waugb28d9xa015626",
-            error: ''
-        }
-    }
+import { Link } from 'react-router-dom';
+import stockImage from './audi-stock-image.jpg'
+import Form from "../Form/Form.jsx"
 
 
+const Login = (props) => {
 
-    handleChange = (e) => {
-        this.setState({[e.target.name]: e.target.value})
-    }
-
-    loginUser = (e) => {
-        // this.state.year === "" || this.state.vin === "" ? 
-        // e.preventDefault() && this.setState({error: "Please Fill In All Inputs"}) 
-        // : this.props.buildCar(decodeVin(vin, year))
-
-        if(this.state.year === "" || this.state.vin === "") {
-            e.preventDefault();
-            this.setState({error: "Please Fill In All Inputs"}) 
-        } else {
-            e.preventDefault();
-            console.log(this.state.vin, this.state.year);
-            
-            this.props.buildCar(decodeVin(this.state.vin, this.state.year))
-            this.setState({vin:"", year:"", error: ""})
-        }
-
-    }
-
-    render() {
 
         
         return (
             <section className='login-container'>
-                <h1>Dream Garage</h1>
-            <form className="login-form" onSubmit= {(e)=> this.loginUser(e)}>
-            <input
-                type='number'
-                minLength ="4"
-                maxLength = "4"
-                placeholder='XXXX'
-                value={this.state.year}
-                name='year'
-                onChange={this.handleChange}
-            />
-            <input
-                type='text'
-                placeholder='vin number'
-                value={this.state.vin}
-                name='vin'
-                onChange={this.handleChange}
-            />
-                <button type='submit' className="submit-btn" >Submit</button>
-            </form>
-            {this.state.error && <div> <p className="error-message">{this.state.error}</p> </div>}
-        {Object.keys(this.props.potentialCar).length > 0  ? 
+                {Object.keys(props.potentialCar).length === 0 ? <Form buildCar ={props.buildCar} /> : ""}
+             
+        
+        {Object.keys(props.potentialCar).length > 0  ? 
+        
         <div className="potential-car-card">
-            <p>{this.props.potentialCar["Model Year"]}</p>
-            <p>{this.props.potentialCar["Make"]}</p>
-            <p>{this.props.potentialCar["Model"]}</p>
-            <div>
-                <button onClick = {() => this.props.addCarToGarage()}>Add To garage</button>
-                <button onClick= {() => window.location.reload(false)}>Start Over</button>
+                <img className="stock-car-image" src={stockImage} alt="stock car"></img>
+            <div className="year-make-model-popup">
+                <p>{props.potentialCar["Model Year"]}</p>
+                <p>{props.potentialCar["Make"]}</p>
+                <p>{props.potentialCar["Model"]}</p>
+            </div>
+            <div className="decision-btns-container">
+           
+                <Link to = '/garage'>
+                <button className="decision-btn" onClick = {() => props.addCarToGarage()}>Add To garage</button>
+                </Link>
+                
+                <button className="decision-btn" onClick= {() => window.location.reload(false)}>Start Over</button>
+           
+           
             </div>
         </div> : null }
             </section>
@@ -78,8 +41,6 @@ class Login extends Component {
         )
     }
 
-
-}
 
 
 export default Login
