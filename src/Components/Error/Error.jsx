e3cfc6be1bd6ccd2af1365ff1008a5823bd08b7d
Taras Tarlov <ttarlov@gmail.com>
2020-06-08 21:40:57 -0600

    Add styling and image to Error comp

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import accident from './error-accident.png'
+import './Error.css'
 
 const Error = () => {
 
     return (
-        <div>
-            <p>Page not found</p>
+        <div className="error-page-container">
+            <img  className="broken-car-img" src={accident} alt="broken car"></img>
+            <p>Accidents Happen</p>
 
             <Link to='/' >
                 <button className='back-to-login-btn'>Go Back to Login Page</button>
