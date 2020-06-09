4243cb455bb4c12135504811a305328d4508a004
Taras Tarlov <ttarlov@gmail.com>
2020-06-09 09:20:40 -0600

    Fix typo error

--- src/Components/CarDetails/CarDetails.jsx
+++ src/Components/CarDetails/CarDetails.jsx
@@ -21,8 +21,9 @@ return (
         <div className="specs-history-container">
             <p>Specs:</p>
             <ul>
+                <li>Model: {props.model === null ? "N/A" : props.model}</li>
                 <li>Trim: {props.trim === null ? "N/A" : props.trim}</li>
-                <li>Type: {props.series === null ? "N/A" : props.trim}</li>
+                <li>Type: {props.series === null ? "N/A" : props.series}</li>
                 <li>Engine Size: {props.displacement === null ? "N/A" : props.displacement}</li>
                 <li>HP: {props.hp === null ? "N/A" : props.hp}</li>
             </ul>
@@ -30,7 +31,6 @@ return (
         </div>
             <div className="history-btn-container">
             <Link to={`/oilchanges/${props.carId}`}>
-                {/* <button id={props.id} className="history-btn" onClick={(e) => props.getOilChangeHistory(e.target.id)}> Oil Change History</button> */}
                 <button id={props.carId} className="history-btn" > Oil Change History</button>
             </Link>
             <Link to='/milage'>
