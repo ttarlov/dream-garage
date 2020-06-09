8a5107ae059eeeb3e91d1d27b18364b65bf0748b
Taras Tarlov <ttarlov@gmail.com>
2020-06-09 09:05:45 -0600

    Remove unused methods

--- src/Components/HistoryCard/HistoryCard.test.js
+++ src/Components/HistoryCard/HistoryCard.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render,fireEvent, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 import HistoryCard from './HistoryCard.jsx'
