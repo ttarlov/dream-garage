5f00b51723a706049ea13e17276a4c608cee86bc
Taras Tarlov <ttarlov@gmail.com>
2020-06-08 22:46:26 -0600

    Add historyCard Tests. Pass tests

---
+++ src/Components/HistoryCard/HistoryCard.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render,fireEvent, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import HistoryCard from './HistoryCard.jsx'
+
+describe("HistoryCard Component", () => {
+
+    
+
+    it('Should see oilchange history if there is any', () => {
+        
+        const { getByText, debug } = render(
+            <MemoryRouter>
+            <HistoryCard oilChangeHistory = {[{Date: "06-24-2020", Mileage: 123.567, id: 2020}]} />
+            </MemoryRouter>
+            )
+
+        const recordEl = getByText("Date: 06-24-2020 | Mileage 123.567")
+        expect(recordEl).toBeInTheDocument()
+        
+        
+        
+    });
+
+
+    it('Should give a user notification if there is no oilchange history available', () => {
+        
+        const { getByText, debug } = render(
+            <MemoryRouter>
+            <HistoryCard oilChangeHistory = {[]} />
+            </MemoryRouter>
+            )
+            
+            const noRecordEl = getByText("No Oil Change History Found")
+            expect(noRecordEl).toBeInTheDocument()
+
+            
+        });
+        
+        
+
+
+})
